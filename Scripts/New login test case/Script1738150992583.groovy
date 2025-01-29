fe014274ed1adfa00ec7aaaf620df4fb8c7430eb
pkaushik <pkaushik@SYN-009232.synoptek.local>
2025-01-29 17:18:22 +0530

    login test

---
+++ Scripts/New login test case/Script1738150992583.groovy
@@ -0,0 +1,39 @@
+import static com.kms.katalon.core.checkpoint.CheckpointFactory.findCheckpoint
+import static com.kms.katalon.core.testcase.TestCaseFactory.findTestCase
+import static com.kms.katalon.core.testdata.TestDataFactory.findTestData
+import static com.kms.katalon.core.testobject.ObjectRepository.findTestObject
+import static com.kms.katalon.core.testobject.ObjectRepository.findWindowsObject
+import com.kms.katalon.core.checkpoint.Checkpoint as Checkpoint
+import com.kms.katalon.core.cucumber.keyword.CucumberBuiltinKeywords as CucumberKW
+import com.kms.katalon.core.mobile.keyword.MobileBuiltInKeywords as Mobile
+import com.kms.katalon.core.model.FailureHandling as FailureHandling
+import com.kms.katalon.core.testcase.TestCase as TestCase
+import com.kms.katalon.core.testdata.TestData as TestData
+import com.kms.katalon.core.testng.keyword.TestNGBuiltinKeywords as TestNGKW
+import com.kms.katalon.core.testobject.TestObject as TestObject
+import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
+import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
+import com.kms.katalon.core.windows.keyword.WindowsBuiltinKeywords as Windows
+import internal.GlobalVariable as GlobalVariable
+import org.openqa.selenium.Keys as Keys
+
+WebUI.openBrowser('')
+
+WebUI.navigateToUrl('https://www.imdb.com/ap/signin?openid.pape.max_auth_age=0&openid.return_to=https%3A%2F%2Fwww.imdb.com%2Fregistration%2Fap-signin-handler%2Fimdb_us&openid.identity=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0%2Fidentifier_select&openid.assoc_handle=imdb_us&openid.mode=checkid_setup&siteState=eyJvcGVuaWQuYXNzb2NfaGFuZGxlIjoiaW1kYl91cyIsInJlZGlyZWN0VG8iOiJodHRwczovL3d3dy5pbWRiLmNvbS8_cmVmXz1sb2dpbiJ9&openid.claimed_id=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0%2Fidentifier_select&openid.ns=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0&tag=imdbtag_reg-20')
+
+WebUI.waitForElementPresent(findTestObject('Page_IMDb Sign-In/input_Email or mobile phone number_email'), 50000)
+
+WebUI.setText(findTestObject('Object Repository/Page_IMDb Sign-In/input_Email or mobile phone number_email'), 'pratikkaushik619@gmail.com')
+
+WebUI.setText(findTestObject('Object Repository/Page_IMDb Sign-In/input_Forgot password_password'), 'arpitamadam')
+
+WebUI.click(findTestObject('Object Repository/Page_IMDb Sign-In/input_Enter your password_signInSubmit'))
+
+WebUI.waitForElementPresent(findTestObject('Page_IMDb Ratings, Reviews, and Where to Wa_0644cb/span_pratikkaushik'), 50000)
+
+WebUI.click(findTestObject('Object Repository/Page_IMDb Ratings, Reviews, and Where to Wa_0644cb/span_pratikkaushik'))
+
+WebUI.click(findTestObject('Object Repository/Page_IMDb Ratings, Reviews, and Where to Wa_0644cb/span_Sign out'))
+
+WebUI.closeBrowser()
+
