0c6dbdb30566b6c3dd7c01f069ff9b5c358e8ea1
pkaushik <pkaushik@SYN-009232.synoptek.local>
2025-01-29 17:18:38 +0530

    repsetory for login

---
+++ Object Repository/Page_IMDb Ratings, Reviews, and Where to Wa_0644cb/span_Sign out.rs
@@ -0,0 +1,124 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>span_Sign out</name>
+   <tag></tag>
+   <elementGuidId>d41c0fac-4061-4f5f-8c70-51a02dd8474f</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>CSS</key>
+         <value>a.ipc-list__item.imdb-header-account-menu__sign-out > span.ipc-list-item__text</value>
+      </entry>
+      <entry>
+         <key>XPATH</key>
+         <value>//span[@id='navUserMenu-contents']/ul/a[6]/span</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <smartLocatorEnabled>false</smartLocatorEnabled>
+   <useRalativeImagePath>true</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>tag</name>
+      <type>Main</type>
+      <value>span</value>
+      <webElementGuid>ad10309f-5bee-4a5e-ac88-7979580f64bd</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>class</name>
+      <type>Main</type>
+      <value>ipc-list-item__text</value>
+      <webElementGuid>9fb03f50-9f84-4996-996c-9bf8d1379261</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>role</name>
+      <type>Main</type>
+      <value>presentation</value>
+      <webElementGuid>972637c3-2a6e-47a9-ab5b-8224427c4abe</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>text</name>
+      <type>Main</type>
+      <value>Sign out</value>
+      <webElementGuid>151a8752-4e00-4fca-89e1-19dafce11fd8</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>id(&quot;navUserMenu-contents&quot;)/ul[@class=&quot;ipc-list imdb-header__account-menu ipc-list--baseAlt&quot;]/a[@class=&quot;ipc-list__item imdb-header-account-menu__sign-out&quot;]/span[@class=&quot;ipc-list-item__text&quot;]</value>
+      <webElementGuid>b4d331d2-63c9-4da3-b5a6-ac353035a4b5</webElementGuid>
+   </webElementProperties>
+   <webElementXpaths>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:idRelative</name>
+      <type>Main</type>
+      <value>//span[@id='navUserMenu-contents']/ul/a[6]/span</value>
+      <webElementGuid>9af5a556-de54-4f04-950c-08d0a2e559ac</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Account settings'])[1]/following::span[1]</value>
+      <webElementGuid>fd66a3a9-ee86-4750-90c4-2f70ea4ae613</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Your lists'])[1]/following::span[2]</value>
+      <webElementGuid>7c5ebd6e-d23e-4d54-9b08-b26fe68431af</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='EN'])[1]/preceding::span[1]</value>
+      <webElementGuid>51dafe22-fd90-4fc3-8213-122b76d192b0</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Fully supported'])[2]/preceding::span[2]</value>
+      <webElementGuid>b0306de9-8929-4643-b75f-c65f14946ef9</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>//*/text()[normalize-space(.)='Sign out']/parent::*</value>
+      <webElementGuid>24f095b8-cb07-4ff9-ad30-8c757b5a319c</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:position</name>
+      <type>Main</type>
+      <value>//span/ul/a[6]/span</value>
+      <webElementGuid>813aa80e-3941-41d5-a160-ee79c224e323</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:customAttributes</name>
+      <type>Main</type>
+      <value>//span[(text() = 'Sign out' or . = 'Sign out')]</value>
+      <webElementGuid>a57dfa44-c1e7-40bf-8d9e-8a1077951521</webElementGuid>
+   </webElementXpaths>
+</WebElementEntity>
